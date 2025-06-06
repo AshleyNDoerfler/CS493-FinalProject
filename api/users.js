648c068509155fb8ebc5861d1e2f2cded2bded62
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2025-06-05 22:57:51 -0700

    Create POST '/' and required model functions for it

---
+++ api/users.js
@@ -0,0 +1,77 @@
+const { Router } = require('express')
+const jwt = require('jsonwebtoken')
+
+const { insertNewUser, UserSchema } = require('../models/user')
+const { validateAgainstSchema } = require('../lib/validation')
+
+const router = Router()
+
+const jwtKey = process.env.JWT_SECRET_KEY;
+
+function requireAuthorization(req, res, next){
+  try{
+    const auth_value = req.get('Authorization')
+
+    if(!auth_value || !auth_value.startsWith("Bearer")) {
+      return res.status(401).send("Incorrect Token")
+    }
+
+    const token = auth_value.split(" ")[1]
+
+    const payload = jwt.verify(token, jwtKey)
+
+    console.log("Payload " + payload)
+
+    req.user = payload.sub
+    next()
+  } catch (err) {
+    res.status(403).send("Unable to Authorize User")
+    next(err)
+  }
+}
+
+/*
+ * Authorizes a user
+ * 
+ * Parameters:
+ *  allowedRoles - who is authorized for this action? (Student, admin, instructor)
+ * 
+ * ex: 
+ *  isAuthorizedUser('admin')
+ */
+function isAuthorizedUser(...allowedRoles) {
+    return function (req, res, next) {
+      const userId = req.params.userId
+      const authenticatedUser = req.user
+  
+      if (!authenticatedUser || !authenticatedUser.id || !authenticatedUser.role) {
+        return res.status(401).json({ error: 'Unauthorized' })
+      }
+  
+      const isOwner = userId && authenticatedUser.id === userId
+      const isAllowedRole = allowedRoles.includes(authenticatedUser.role)
+  
+      if (isOwner || isAllowedRole) {
+        next()
+      } else {
+        res.status(403).json({ error: 'Insufficient privileges' })
+      }
+    }
+  }
+  
+
+/*
+ * Authenticated Admin can create a User
+ */
+router.post('/users', requireAuthorization, isAuthorizedUser('admin'), async (req, res) => {
+    if (!validateAgainstSchema(req.body, UserSchema)) {
+        return res.status(400).json({ error: "Missing or invalid user fields" })
+    }
+
+    try{
+        const user = await insertNewUser(req.body)
+        res.status(201).send({ id: user.id })
+    } catch (e) {
+        console.error(e)
+    }
+})
---
+++ models/users.js
@@ -0,0 +1,51 @@
+/*
+ * User schema and data accessor methods
+ */
+
+const bcrypt = require('bcrypt');
+
+const { ObjectId } = require('mongodb')
+
+const { getDbReference } = require('../lib/mongo')
+const { extractValidFields } = require('../lib/validation')
+
+/*
+ * Schema describing required/optional fields of a User object.
+ */
+const UserSchema = {
+  name: { required: true }, 
+  email: { required: true }, 
+  password: { required: true }, 
+  role: { required: true }, 
+}
+exports.UserSchema
+
+const UserClientFields = ['name', 'email', 'password', 'role']
+exports.UserClientFields = UserClientFields
+
+/*
+ * Insert new User into the database
+ */
+async function insertNewUser(userData) {
+  const db = getDbReference()
+  const collection = db.collection('users')
+
+  const newUser = extractValidFields(userData, UserSchema)
+  
+  newUser.password = bcrypt.hash(newUser.password, 8)
+  const result = await collection.insertOne(newUser)
+  return result.insertedId
+}
+
+exports.insertNewUser = insertNewUser
+
+/*
+ * Get a User from the database by id
+ */
+async function getUserById(id) {
+  const db = getDbReference()
+  const collection = db.collection('users')
+  return await collection.findOne({ _id: new ObjectId(id) })
+}
+
+exports.getUserById = getUserById
